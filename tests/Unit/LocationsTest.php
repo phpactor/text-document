d376ce2201c58aa04d610e209d8af102287a2025
Camille Dejoye <camilledejoye@gmail.com>
2020-10-11 16:59:00 +0200

    can sort Locations
    Make the class final to be able to use "new self" without unexpecting
    results in case of inheritance.
    Acceps any iterable instead of only array to be more compliant.

--- tests/Unit/LocationsTest.php
+++ tests/Unit/LocationsTest.php
@@ -9,7 +9,7 @@ use RuntimeException;
 
 class LocationsTest extends TestCase
 {
-    public function testContainsLocations()
+    public function testContainsLocations(): void
     {
         $locations = new Locations([
             Location::fromPathAndOffset('/path/to.php', 12),
@@ -18,7 +18,7 @@ class LocationsTest extends TestCase
         $this->assertCount(2, $locations);
     }
 
-    public function testIsCountable()
+    public function testIsCountable(): void
     {
         $locations = new Locations([
             Location::fromPathAndOffset('/path/to.php', 12),
@@ -27,7 +27,7 @@ class LocationsTest extends TestCase
         $this->assertEquals(2, $locations->count());
     }
 
-    public function testExceptionIfFirstNotAvailable()
+    public function testExceptionIfFirstNotAvailable(): void
     {
         $this->expectException(RuntimeException::class);
         $locations = new Locations([
@@ -35,7 +35,7 @@ class LocationsTest extends TestCase
         $locations->first();
     }
 
-    public function testAppendLocations()
+    public function testAppendLocations(): void
     {
         $locations = new Locations([
             Location::fromPathAndOffset('/path/to.php', 12),
@@ -48,5 +48,52 @@ class LocationsTest extends TestCase
             Location::fromPathAndOffset('/path/to.php', 12),
             Location::fromPathAndOffset('/path/to.php', 13)
         ]), $locations);
+    }
+
+    public function testCreateSortedLocations(): void
+    {
+        $locationList = [
+            Location::fromPathAndOffset('/path/to.php', 12),
+            Location::fromPathAndOffset('/path/from.php', 13),
+            Location::fromPathAndOffset('/path/to.php', 15),
+        ];
+        $locations = Locations::bySorting($locationList);
+
+        $this->assertEquals([
+            $locationList[1],
+            $locationList[0],
+            $locationList[2],
+        ], iterator_to_array($locations));
+    }
+
+    /**
+     * @dataProvider provideUnsortedLocations
+     *
+     * @param Location[] $unsortedLocations
+     * @param Location[] $sortedLocations
+     */
+    public function testSortLocations(
+        array $unsortedLocations,
+        array $sortedLocations
+    ): void {
+        $locations = new Locations($unsortedLocations);
+
+        $this->assertEquals($sortedLocations, iterator_to_array($locations->sort()));
+    }
+
+    /**
+     * @return iterable<array>
+     */
+    public function provideUnsortedLocations(): iterable
+    {
+        yield '2 files and 3 references' => [[
+            Location::fromPathAndOffset('/path/to.php', 15),
+            Location::fromPathAndOffset('/path/to.php', 12),
+            Location::fromPathAndOffset('/path/from.php', 13),
+        ], [
+            Location::fromPathAndOffset('/path/from.php', 13),
+            Location::fromPathAndOffset('/path/to.php', 12),
+            Location::fromPathAndOffset('/path/to.php', 15),
+        ]];
     }
 }
